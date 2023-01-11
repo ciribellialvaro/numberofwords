13fd297abf95cb4b9165dc50928a515134d9dd71
Álvaro Borges <gestor.alvaro@gmail.com>
2023-01-10 23:42:34 -0300

    Add project files.

---
+++ numberofwords/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace numberofwords
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Escreva um texto: ");
+            var texto = Console.ReadLine();
+
+            var words = texto.Split(' ');
+            var totalWords = new List<string>();   
+
+            foreach (string word in words)
+            {
+
+                totalWords.Add(word);
+            }
+            Console.WriteLine(totalWords.Count);
+
+        }
+    }
+}
